ee48eb47d6c0b9ed11c14dc29fe00cd1de779aeb
JJulia9 <juliaworkjj@gmail.com>
2024-05-15 12:01:47 +0100

    cubDash

---
+++ my-project/src/components/ProfileCard.jsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import UserInformation from "./UserInformation";
+
+
+
+const ProfileCard = () => {
+    const user = UserInformation()
+    
+    const formatDOB = (dob) => {
+        const formattedDOB = new Date(dob).toLocaleDateString('en-GB');
+        const [day, month, year] = formattedDOB.split('/');
+        return `${day}.${month}.${year}`;
+      }; 
+
+    return (
+       
+        <>
+
+        {user && (
+
+        <div className='m-10'>
+        <h3 className="text-primary font-heading text-xl sm:text-2xl font-medium max-w-screen-xl mx-auto pb-5 p-2"> Profile </h3>
+        <div className="h-64 max-w-screen-xl mx-auto">
+        <div className="bg-white p-3 shadow-m border-secondary border-2 border-solid rounded-xl">
+            <div className="flex items-center space-x-2 font-semibold text-nav font-paragraph">
+                <span className="text-secondary">
+                    <svg className="h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                    </svg>
+                </span>
+                <span className="tracking-wide">About</span>
+            </div>
+            <div className="text-nav font-paragraph">
+                <div className="grid md:grid-cols-2 text-sm">
+                    <div className="grid grid-cols-2">
+                        <div className="px-4 py-2 font-semibold">First Name</div>
+                        <div className="px-4 py-2">{user.firstName}</div>
+                    </div>
+                    <div className="grid grid-cols-2">
+                        <div className="px-4 py-2 font-semibold">Last Name</div>
+                        <div className="px-4 py-2">{user.lastName}</div>
+                    </div>
+
+                    <div className="grid grid-cols-2">
+                        <div className="px-4 py-2 font-semibold">Birthday</div>
+                        <div className="px-4 py-2">{formatDOB(user.dob)}</div>
+                    </div>
+
+                    <div className="grid grid-cols-2">
+                        <div className="px-4 py-2 font-semibold">Address</div>
+                        <div className="px-4 py-2">{user.address}</div>
+                    </div>
+
+                    <div className="grid grid-cols-2">
+                        <div className="px-4 py-2 font-semibold"> Cub Email</div>
+                        <div className="px-4 py-2">
+                            <a className="text-secondary" href="mailto:jane@example.com">{user.email}</a>
+                        </div>
+                    </div>
+
+
+                    <div className="grid grid-cols-2">
+                        <div className="px-4 py-2 font-semibold">Parent Name</div>
+                        <div className="px-4 py-2">{user.parentName}</div>
+                    </div>
+
+                    <div className="grid grid-cols-2">
+                        <div className="px-4 py-2 font-semibold">Parent Email</div>
+                        <div className="px-4 py-2">
+                            <a className="text-secondary" href="mailto:jane@example.com">{user.parentEmail}</a>
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-2">
+                        <div className="px-4 py-2 font-semibold">Parent Phone</div>
+                        <div className="px-4 py-2">{user.parentPhone}</div>
+                    </div>
+
+                    
+                    <div className="grid grid-cols-2">
+                        <div className="px-4 py-2 font-semibold">Allergies</div>
+                        <div className="px-4 py-2">{user.allergies}</div>
+                    </div>
+
+                    
+                   
+                </div>
+            </div>
+           
+           
+        </div>
+        </div>
+        </div>
+        )}
+
+        </>
+    );
+}
+
+
+
+
+export default ProfileCard;
--- my-project/src/pages/cubs/CubDashboard.jsx
+++ my-project/src/pages/cubs/CubDashboard.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import Navigation from '../../components/Navigation';
+import Footer from '../../components/Footer';
+import UploadForm from '../../components/UploadForm';   
+import ProfileCard from '../../components/ProfileCard';
+
+
 
 const CubDashboard = () => {
+
+
     return (
-        <div>
-            <h1>Cub Dashboard</h1>
+        <>
+        <Navigation />
+        <div className='bg-background'>
+        <div className="w-full py-10  bg-secondary my-10  flex-row">
+        <div className="flex max-w-screen-xl mx-auto">
+        <h1 className="font-heading  text-white text-4xl p-2 mb-2">Welcome back Cub </h1>
+        <svg className="w-38 h-38 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="none" viewBox="0 0 38 38">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14.99 9H15M8.99 9H9m12 3a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM7 13c0 1 .507 2.397 1.494 3.216a5.5 5.5 0 0 0 7.022 0C16.503 15.397 17 14 17 13c0 0-1.99 1-4.995 1S7 13 7 13Z"/>
+        </svg>
         </div>
+    </div>
+
+         <ProfileCard />
+
+
+         <UploadForm />
+        </div>
+        <Footer />
+        </>
     )
 }
 
